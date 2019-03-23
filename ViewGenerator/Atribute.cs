69151de0f788bc5ab3ec7a6364e1f37de47e17da
Tomislav <Tomislav@DESKTOP-VF53KDE>
2019-03-23 17:07:58 +0100

    data types

---
+++ ViewGenerator/Atribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ViewGenerator
+{
+    public class Atribute
+    {
+        public string name { get; set; }
+        public string value {get; set;}
+        public string type { get; set; }
+        public bool hidden { get; set; }
+        public bool validationNeeded { get; set; }
+        public bool nullable { get; set; }
+
+        public Atribute() { }
+    }
+}
---
+++ ViewGenerator/ViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ViewGenerator
+{
+    class ViewModel
+    {
+        public string dbTable { get; set; }
+        public List<Atribute> atributes { get; set; }
+    }
+}
