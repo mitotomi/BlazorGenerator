295b5fb31ec9ebd5bf671c5d8b338629d6060d62
Tomislav <Tomislav@DESKTOP-VF53KDE>
2019-03-24 17:56:43 +0100

    model

---
+++ Master_v2.Server/Models/OrderMasterContext.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Master_v2.Shared.Models;
+
+namespace Master_v2.Server.Models
+{
+    public partial class OrderMasterContext : DbContext
+    {
+        public OrderMasterContext()
+        {
+        }
+
+        public OrderMasterContext(DbContextOptions<OrderMasterContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Person> Person { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Data Source=DESKTOP-VF53KDE;Initial Catalog=OrderMaster;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Person>(entity =>
+            {
+                entity.Property(e => e.Id).ValueGeneratedNever();
+
+                entity.Property(e => e.Address).HasMaxLength(100);
+
+                entity.Property(e => e.BirthDate).HasColumnType("date");
+
+                entity.Property(e => e.Gender)
+                    .IsRequired()
+                    .HasMaxLength(1)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.LastName)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(30);
+
+                entity.Property(e => e.Oib)
+                    .IsRequired()
+                    .HasColumnName("OIB")
+                    .HasMaxLength(11);
+            });
+        }
+    }
+}
---
+++ Master_v2.Shared/Models/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Master_v2.Shared.Models
+{
+    public partial class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Oib { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Address { get; set; }
+        public string Gender { get; set; }
+    }
+}
